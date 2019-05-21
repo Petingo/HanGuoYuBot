be146ca7dc72e5e474fb437a5015e2cc1a0aabe7
Petingo <petingo@ip137-201.wlan.ntust.edu.tw>
2019-05-21 23:09:50 +0800

    change listing port

--- HanGuoYuBot.js
+++ HanGuoYuBot.js
@@ -7,6 +7,7 @@ bot.on("message", (msg, metadata) => {
     bot.sendMessage(msg.chat.id, "高雄發大財！");
 });
 
+// check alive
 const http = require('http');
 var server = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
