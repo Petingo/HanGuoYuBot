55d7f8f06d46b8732375aff58bd57d9bf1df0fce
Petingo <petingo@ip137-201.wlan.ntust.edu.tw>
2019-05-21 23:04:05 +0800

    add test

--- HanGuoYuBot.js
+++ HanGuoYuBot.js
@@ -6,3 +6,12 @@ const bot = new TelegramBot(token, { polling: true });
 bot.on("message", (msg, metadata) => {
     bot.sendMessage(msg.chat.id, "高雄發大財！");
 });
+
+const http = require('http');
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write("I'm alive");
+    res.end();
+});
+ 
+server.listen(8888);
